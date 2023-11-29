382d9ae1bfee4f368744f560fd50c651b12657e5
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-11-29 19:54:50 +0000

    Added image watermark on the middle of the original image

--- app.js
+++ app.js
@@ -12,6 +12,30 @@ const addTextWatermarkToImage = async function (inputFile, outputFile, text) {
 	await image.quality(100).writeAsync(outputFile);
 };
 
+const addImageWatermarkToImage = async function (
+	inputFile,
+	outputFile,
+	watermarkFile
+) {
+	const image = await Jimp.read(inputFile);
+	const watermark = await Jimp.read(watermarkFile);
+
+	const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+	const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+
+	image.composite(watermark, x, y, {
+		mode: Jimp.BLEND_SOURCE_OVER,
+		opacitySource: 0.5,
+	});
+	await image.quality(100).writeAsync(outputFile);
+};
+
+addImageWatermarkToImage(
+	'./img/test.jpg',
+	'./img/test-with-watermark2.jpg',
+	'./img/logo.png'
+);
+
 addTextWatermarkToImage(
 	'./img/test.jpg',
 	'./img/test-with-watermark.jpg',
